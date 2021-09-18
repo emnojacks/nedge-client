2d8cdec929e0ab1736a129d8788c69f1ed8fedd4
Emily Jackson <emj@Emilys-Air.lan>
2021-09-18 00:35:11 -0400

    starting to build structure of compoents

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+interface Props {
+    
+}
+ 
+interface State {
+    
+}
+ 
+class Login extends Component<Props, State> {
+    state = { :  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Login;
